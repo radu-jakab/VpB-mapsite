f8cb2d3299d416327601043421253db6ec938fd1
Radu Jakab <radu.m.jakab@gmail.com>
2018-07-05 12:52:28 +0300

    other fixed to set on cycle-lane context

--- mapsite.js
+++ mapsite.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('#example').DataTable({
-        ajax: "/download/piste_datatable_1.0.json",
+        ajax: "/cycle-lane/download/piste_datatable_1.0.json",
         columnDefs: [
             {"className": "dt-center", "targets": "_all"}
         ],
