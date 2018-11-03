a1a763c21074d9efb729a8364552ee2e6234c0a6
Radu Jakab <radu.m.jakab@gmail.com>
2018-11-03 13:10:51 +0200

    data & design updates

--- wp/wp_content/themes/pedaleaza-child/assets/js/mapsite.js
+++ wp/wp_content/themes/pedaleaza-child/assets/js/mapsite.js
@@ -1,20 +1,21 @@
 $(document).ready(function () {
-    
+
     var table = $('#example').DataTable({
         ajax: "/wp-content/themes/pedaleaza-child/assets/download/current/piste_datatable.json",
         columnDefs: [
             {"className": "dt-center", "targets": "_all"}
         ],
         paging: false,
-        rowCallback: function(row, data, index) {
+        order: [[8, "desc"]],
+        rowCallback: function (row, data, index) {
             var order = table.order();
             console.log(row);
             //alert( 'Column '+order[0][0]+' is the ordering column' );
             var orderingCol = order[0][0];
-            if (orderingCol < 6){
+            if (orderingCol < 6) {
                 orderingCol = 8;
             }
-            if(orderingCol > 4){
+            if (orderingCol > 4) {
                 if (0 <= data[orderingCol] && data[orderingCol] < 20) {
                     $(row).removeClass('rowTotalTwenty rowTotalForty rowTotalSixty rowTotalEighty rowTotalHundred');
                     $(row).addClass('rowTotalTwenty');
