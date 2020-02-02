c6437d5411e368e28d770ce73bb0c265eb9ac6ec
Radu Jakab <radu.m.jakab@gmail.com>
2020-02-02 21:09:15 +0200

    main page now merges 2018 with 2019 data

--- static/wp_content/themes/pedaleaza-child/assets/js/mapsite.js
+++ static/wp_content/themes/pedaleaza-child/assets/js/mapsite.js
@@ -1,44 +1,78 @@
 $(document).ready(function () {
+    Promise.all(
+        [loadYearData('./wp_content/themes/pedaleaza-child/assets/download/current/piste_datatable.json'),
+            loadYearData('./wp_content/themes/pedaleaza-child/assets/download/current/piste_datatable_2019.json')]
+    ).then((values) => {
+        const merged = merge2018with2019(values[0].data, values[1].data);
 
-    var table = $('#example').DataTable({
-        ajax: "./wp_content/themes/pedaleaza-child/assets/download/current/piste_datatable_2019.json",
-        columnDefs: [
-            {"className": "dt-center", "targets": "_all"}
-        ],
-        paging: false,
-        order: [[8, "desc"]],
-        rowCallback: function (row, data, index) {
-            var order = table.order();
-            console.log(row);
-            //alert( 'Column '+order[0][0]+' is the ordering column' );
-            var orderingCol = order[0][0];
-            if (orderingCol < 6) {
-                orderingCol = 8;
-            }
-            if (orderingCol > 4) {
-                if (0 < data[orderingCol] && data[orderingCol] < 20) {
+        $('#example').DataTable({
+            data: merged,
+            columnDefs: [
+                {"className": "dt-center", "targets": "_all"}
+            ],
+            paging: false,
+            order: [[6, "desc"]],
+            rowCallback: function (row, data) {
+                const columnTotal2019 = 6;
+                if (0 < data[columnTotal2019] && data[columnTotal2019] < 20) {
                     $(row).removeClass('rowTotalTwenty rowTotalForty rowTotalSixty rowTotalEighty rowTotalHundred');
                     $(row).addClass('rowTotalTwenty');
                 }
-                if (20 <= data[orderingCol] && data[orderingCol] < 40) {
+                if (20 <= data[columnTotal2019] && data[columnTotal2019] < 40) {
                     $(row).removeClass('rowTotalTwenty rowTotalForty rowTotalSixty rowTotalEighty rowTotalHundred');
                     $(row).addClass('rowTotalForty');
                 }
-                if (40 <= data[orderingCol] && data[orderingCol] < 60) {
+                if (40 <= data[columnTotal2019] && data[columnTotal2019] < 60) {
                     $(row).removeClass('rowTotalTwenty rowTotalForty rowTotalSixty rowTotalEighty rowTotalHundred');
                     $(row).addClass('rowTotalSixty');
                 }
-                if (60 <= data[orderingCol] && data[orderingCol] < 80) {
+                if (60 <= data[columnTotal2019] && data[columnTotal2019] < 80) {
                     $(row).removeClass('rowTotalTwenty rowTotalForty rowTotalSixty rowTotalEighty rowTotalHundred');
                     $(row).addClass('rowTotalEighty');
                 }
-                if (80 <= data[orderingCol] && data[orderingCol] < 100) {
+                if (80 <= data[columnTotal2019] && data[columnTotal2019] < 100) {
                     $(row).removeClass('rowTotalTwenty rowTotalForty rowTotalSixty rowTotalEighty rowTotalHundred');
                     $(row).addClass('rowTotalHundred');
                 }
-            }
-        },
+            },
+        });
     });
 });
 
+function merge2018with2019(json2018, json2019) {
+    json2018.forEach(item2018 => {
+        const urlAsId = item2018[0];
+        const foundMatch = json2019.filter(item => item[0] === urlAsId);
+        if (foundMatch[0]) {
+            foundMatch[0].splice(8, 0, item2018[8]); // add score from 2018
+            foundMatch[0].push(Number.parseFloat(foundMatch[0][9]) - Number.parseFloat(item2018[8])); // add score difference
+        } else {
+            item2018.push('-'); // add score from 2018
+            item2018.push(0);
+            json2019.push(item2018);
+        }
+    });
+    json2019.forEach(item => {
+        if (item.length < 10) {
+            item.splice(8, 0, '-'); // add score from 2018
+            item.push(0);
+        }
+        item.splice(5, 3);
+    }); // remove category scores
+    return json2019;
+}
 
+function loadYearData(fileURL) {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.open('GET', fileURL, true);
+        request.onload = function () {
+            try {
+                resolve(JSON.parse(request.response.toString()));
+            } catch (e) {
+                reject(e);
+            }
+        };
+        request.send();
+    });
+}
